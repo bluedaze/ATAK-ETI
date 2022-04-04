360ac90673744f48c14a3e9f0e10266a85ee4c7d
bluedaze <35676690+bluedaze@users.noreply.github.com>
2022-04-04 11:10:47 -0400

    Add files via upload

--- databaseController.py
+++ databaseController.py
@@ -3,18 +3,21 @@ import face_recognition
 import gridfs
 import datetime
 from datetime import datetime
-from datetime import date
+import io, base64
+import os
+import sys
 from PIL import Image, UnidentifiedImageError
-import io
 
 class DB():
     def __init__(self):
         self.client = MongoClient("mongodb://127.0.0.1:27017")
+        self.registered_users = self.get_registrants()
+        db = self.client.gridfs_images
+        self.fs = gridfs.GridFS(db)
 
     def send_to_log(self, json, database_name):
-        db = self.client.gridfs_images
-        fs = gridfs.GridFS(db)
-        put_image = fs.put(json["image"])
+        b64 = base64.b64decode(json["image"])
+        put_image = self.fs.put(b64)
         post = {"image": put_image,
                 "date": datetime.now(),
                 "name": json["name"],
@@ -23,49 +26,51 @@ class DB():
         collection = registration['faces']
         collection.insert_one(post)
 
-    def recognize_images(self, sent_image):
-        # This is the sloppiest code I've ever written.
-        # There is a better way to do this, but I'm in a rush.
-        time = datetime.now().strftime("%H:%M:%S")
-        current_date = date.today().strftime("%Y-%m-%d")
-        items = self.get_registrants()
-        db = self.client.gridfs_images
-        fs = gridfs.GridFS(db)
-        for count, item in enumerate(items):
-            results = [None]
-            item["time"] = time
-            item["date"] = current_date
-            img = fs.get(item["image"]).read()
-            img = Image.open(io.BytesIO(img))
-            img.save("testImage.png")
-            known_image = face_recognition.load_image_file("testImage.png")
-            # Assume the whole image is the location of the face
-            height, width, _ = known_image.shape
-            # location is in css order - top, right, bottom, left
-            face_location = (0, width, height, 0)
-            try:
-                unknown_image = face_recognition.load_image_file("temp.png")
-                known_encoding = face_recognition.face_encodings(known_image, known_face_locations=[face_location])[0]
-                unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-                results = face_recognition.compare_faces([known_encoding], unknown_encoding)
-            except UnidentifiedImageError:
-                print("Image sucks, or is broken. Most likely broken")
-            except IndexError:
-                print("Image is missing a face.")
-            if results[0]:
-                item["image"] = sent_image
-                self.send_to_log(item, 'recognized_faces')
-                self.send_to_log(item, 'detected_faces')
-                print(f"Name: {item['name']}\nID: {item['id_num']}")
-                data = {"Name": item["name"], "ID_NUM": item["id_num"]}
-                return data
-            else:
-                print("UNKNOWN")
-                item["name"] = "UNKNOWN"
-                item["id_num"] = "UNKNOWN"
-                item["image"] = sent_image
-                self.send_to_log(item, 'detected_faces')
-                return False
+    def get_user_image(self, user):
+        user_image = self.fs.get(user["image"]).read()
+        user_image = Image.open(io.BytesIO(user_image))
+        user_image.save("known_user.png")
+        known_user = face_recognition.load_image_file("known_user.png")
+        return known_user
+
+    def decode_image(self, base64_string, name):
+        # Convert from base64 string to base64 bytes
+        b64 = base64.b64decode(base64_string)
+        # Convert to b64 binary object.
+        base64_bytes = base64.b64encode(b64)
+        with open(name, "wb") as fh:
+            fh.write(base64.decodebytes(base64_bytes))
+            return fh
+
+    def get_unknown_image(self, user_request):
+        self.decode_image(user_request["image"], "unknown_user.png")
+        unknown_image = face_recognition.load_image_file("unknown_user.png")
+        return unknown_image
+
+    def compare_images(self, known_image, unknown_user):
+        # Assume the whole image is the location of the face
+        height, width, _ = known_image.shape
+        # location is in css order - top, right, bottom, left
+        face_location = (0, width, height, 0)
+        known_encoding = face_recognition.face_encodings(known_image, known_face_locations=[face_location])[0]
+        unknown_encoding = face_recognition.face_encodings(unknown_user)[0]
+        results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+        return results[0]
+
+    def process_request(self, user_request):
+        for user in self.registered_users:
+            known_user = self.get_user_image(user)
+            unknown_user = self.get_unknown_image(user_request)
+            match = self.compare_images(known_user, unknown_user)
+            if match:
+                user_request["name"] = user["name"]
+                user_request["id_num"] = user["id_num"]
+                self.send_to_log(user_request, 'recognized_faces')
+                break
+        # We send all faces to detected faces, whether it is a match or not.
+        # Then we delete the image, so that it can't be reused again.
+        self.send_to_log(user_request, 'detected_faces')
+        return user_request
 
     def get_registrants(self):
         registration = self.client['registered_users']
@@ -75,14 +80,14 @@ class DB():
 
     def get_logs(self, databaseName):
         rows = []
-        registration = self.client[databaseName]
-        collection = registration['faces']
+        database = self.client[databaseName]
+        collection = database['faces']
         items = collection.find()
         for item in items:
-            registrant = {'Name': item["name"], 'ID': item["id_num"], 'Date': item["date"].strftime("%Y-%m-%d"), 'Time': item["date"].strftime("%H:%M:%S")}
-            rows.append(registrant)
-        return rows
+            user = {'Name': item["name"], 'ID': item["id_num"], 'Date': item["date"].strftime("%Y-%m-%d"), 'Time': item["date"].strftime("%H:%M:%S")}
+            rows.append(user)
+        return rows[::-1]
 
 if __name__ == "__main__":
     mydb = DB()
-    items = mydb.get_registrants()
+    items = mydb.get_logs("detected_faces")
--- views.py
+++ views.py
@@ -2,9 +2,57 @@ from flask import Flask, render_template, request
 from PIL import Image
 import io, base64
 from databaseController import DB
-import csv
 
 app = Flask(__name__)
+db = DB()
+
+
+def decode_base64():
+    ''' Decodes a base 64 image, and saves it as unknown_image.png '''
+    base64_string = request.form.get("image")
+    b = io.BytesIO(base64.b64decode(base64_string))
+    im = Image.open(b)
+    image_bytes = io.BytesIO()
+    im.save(image_bytes, format='PNG')
+    im.save("./unknown_image.png")
+    return image_bytes.getvalue()
+
+def decode_image(base64_string, name):
+    # Convert from base64 string to base64 bytes
+    b64 = base64.b64decode(base64_string)
+    # Convert to b64 binary object.
+    base64_bytes = base64.b64encode(b64)
+    with open(name, "wb") as fh:
+        fh.write(base64.decodebytes(base64_bytes))
+        return fh
+
+@app.route('/verify', methods=['POST'])
+def verify():
+    ''' Checks for a face recognition and then returns a response based on if the face is recognized '''
+    user_request = dict(zip(request.form.keys(), request.form.values()))
+    data = db.process_request(user_request)
+    if data:
+        return data
+    else:
+        return "Record not found", 400
+
+def transaction_template(database_name):
+    logs = db.get_logs(database_name)
+    return render_template(
+        "transactionLogs.html",
+        logs=logs
+    )
+
+@app.route('/save', methods=['POST'])
+def save_new_user():
+    ''' Registers a user to the database '''
+    form_request = dict(zip(request.form.keys(), request.form.values()))
+    # replacement method
+    # base64_string = request.form.get("image")
+    # decode_image(base64_string, "unknown_image.png")
+    image_bytes = decode_base64()
+    form_request["image"] = image_bytes
+    db.send_to_log(form_request, 'registered_users')
 
 @app.route("/")
 @app.route("/index")
@@ -16,7 +64,6 @@ def index():
         "index.html",
     )
 
-
 @app.route("/start")
 def start():
     '''
@@ -26,53 +73,10 @@ def start():
         "start.html",
     )
 
-
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET'])
 def register():
     ''' Registers a user to the database '''
-    if request.method == 'GET':
-        return render_template('register.html')
-    else:
-        id_num = request.form.get("id")
-        name = request.form.get("name")
-        image_bytes = decode_base64()
-        post = {"name": name, "id_num": id_num, "image": image_bytes}
-        mydb = DB()
-        mydb.send_to_log(post, 'registered_users')
-    return "ok"
-
-def decode_base64():
-    ''' Decodes a base 64 image, and saves it as temp.png '''
-    base64_string = request.form.get("image")
-    b = io.BytesIO(base64.b64decode(base64_string))
-    im = Image.open(b)
-    image_bytes = io.BytesIO()
-    im.save(image_bytes, format='PNG')
-    im.save("./temp.png")
-    return image_bytes.getvalue()
-
-def check_recognition(image_bytes):
-    mydb = DB()
-    data = mydb.recognize_images(image_bytes)
-    return data
-
-@app.route('/verify', methods=['POST'])
-def verify():
-    ''' Checks for a face recognition and then returns a response based on if the face is recognized'''
-    decoded_image =  decode_base64()
-    data = check_recognition(decoded_image)
-    if data:
-        return data
-    else:
-        return "Record not found", 400
-
-def transaction_template(database_name):
-    mydb = DB()
-    logs = mydb.get_logs(database_name)
-    return render_template(
-        "transactionLogs.html",
-        logs=logs
-    )
+    return render_template('register.html')
 
 @app.route('/recognitions')
 def recognitions():
