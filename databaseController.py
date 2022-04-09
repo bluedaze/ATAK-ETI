4a0fc8115a6dd2daf6a739cb455c90987b88d5c9
bluedaze <35676690+bluedaze@users.noreply.github.com>
2022-04-08 22:42:33 -0400

    Add files via upload

--- databaseController.py
+++ databaseController.py
@@ -1,10 +1,15 @@
 import base64
 from pymongo import MongoClient
 import face_recognition
+import os
+from pprint import pprint
 import gridfs
+import copy
+import uuid
 import datetime
 from datetime import datetime
 from datetime import date
+from pprint import pprint
 from PIL import Image, UnidentifiedImageError
 import io
 
@@ -15,44 +20,15 @@ class DB():
         self.imgdb = self.client.gridfs_images
         # self.fs = gridfs.GridFS(db)
 
-    def decode_base64(self, base64_string):
-        ''' Decodes a base 64 image, and saves it as unknown_image.png '''
-        b = io.BytesIO(base64.b64decode(base64_string))
-        im = Image.open(b)
-        image_bytes = io.BytesIO()
-        im.save(image_bytes, format='PNG')
-        im.save("./unknown_image.png")
-        return image_bytes.getvalue()
-
-    def send_to_log(self, json, database_name):
-        b64 = base64.b64decode(json["image"])
-        fs = gridfs.GridFS(self.imgdb)
-        put_image = fs.put(b64)
-        post = {"image": put_image,
-                "date": datetime.now(),
-                "name": json["name"],
-                "id_num": json["id_num"]}
+    def send_to_log(self, user_request, database_name):
         registration = self.client[database_name]
         collection = registration['faces']
-        collection.insert_one(post)
-
-    def get_unknown_image(self, user_request):
-        self.decode_image(user_request["image"], "unknown_user.png")
-        unknown_image = face_recognition.load_image_file("unknown_user.png")
-        return unknown_image
-
-    def get_user_image(self, user):
-        self.fs = gridfs.GridFS(self.imgdb)
-        user_image = self.fs.get(user["image"]).read()
-        user_image = Image.open(io.BytesIO(user_image))
-        user_image.save("known_user.png")
-        known_user = face_recognition.load_image_file("known_user.png")
-        return known_user
+        collection.insert_one(user_request)
 
     def compare_images(self, user_request, user):
         try:
-            unknown_image = self.get_unknown_image(user_request)
-            known_image = self.get_user_image(user)
+            unknown_image = face_recognition.load_image_file(f".{user_request['image']}")
+            known_image = face_recognition.load_image_file(f".{user['image']}")
             # Assume the whole image is the location of the face
             height, width, _ = known_image.shape
             # location is in css order - top, right, bottom, left
@@ -60,7 +36,6 @@ class DB():
             known_user = face_recognition.face_encodings(known_image, known_face_locations=[face_location])[0]
             unknown_user = face_recognition.face_encodings(unknown_image)[0]
             results = face_recognition.compare_faces([known_user], unknown_user)
-            print(results)
             return results[0]
         except UnidentifiedImageError:
             print("Image sucks, or is broken. Most likely broken")
@@ -68,48 +43,37 @@ class DB():
             print("Image is missing a face.")
         return None
 
-    def process_request(self, user_request):
-        for user in self.registered_users:
-            known_user = self.get_user_image(user)
-            unknown_user = self.get_unknown_image(user_request)
-            match = self.compare_images(known_user, unknown_user)
-            if match:
-                user_request["name"] = user["name"]
-                user_request["id_num"] = user["id_num"]
-                self.send_to_log(user_request, 'recognized_faces')
-                break
-        # We send all faces to detected faces, whether it is a match or not.
-        # Then we delete the image, so that it can't be reused again.
-        self.send_to_log(user_request, 'detected_faces')
+    def save_image(self, user_request):
+        directory = "static/images"
+        cwd = os.getcwd()
+        base64_string = user_request["image"]
+        unique_id = uuid.uuid1()
+        file = f"{unique_id}.png"
+        location = f"/{directory}/{file}"
+        working_directory = f"{cwd}/{directory}/"
+        completeName = os.path.join(working_directory, file)
+        b64 = base64.b64decode(base64_string)
+        base64_bytes = base64.b64encode(b64)
+        with open(completeName, "wb") as file:
+            file.write(base64.decodebytes(base64_bytes))
+        user_request["image"] = f"{location}"
         return user_request
 
-
     def recognize_images(self, user_request):
-        sent_image = self.decode_base64(user_request["image"])
+        self.save_image(user_request)
         for user in self.registered_users:
             match = self.compare_images(user_request, user)
             if match:
-                user_request["image"] = sent_image
-                user_request["name"] = user["name"]
-                user_request["id_num"] = user["id_num"]
+                user_request["Original"] = user["image"]
+                user_request["Name"] = user["Name"]
+                user_request["ID Number"] = user["ID Number"]
                 self.send_to_log(user_request, 'recognized_faces')
-                data = {"name": user_request["name"], "id_num": user_request["id_num"]}
+                self.send_to_log(user_request, 'detected_faces')
+                data = {"name": user_request["Name"], "ID Number": user_request["ID Number"]}
                 return data
         print("UNKNOWN")
-        user_request["name"] = "UNKNOWN"
-        user_request["id_num"] = "UNKNOWN"
-        user_request["image"] = sent_image
         self.send_to_log(user_request, 'detected_faces')
         return False
-
-    def decode_image(self, base64_string, name):
-        # Convert from base64 string to base64 bytes
-        b64 = base64.b64decode(base64_string)
-        # Convert to b64 binary object.
-        base64_bytes = base64.b64encode(b64)
-        with open(name, "wb") as fh:
-            fh.write(base64.decodebytes(base64_bytes))
-            return fh
 
     def get_registrants(self):
         registration = self.client['registered_users']
@@ -119,14 +83,16 @@ class DB():
 
     def get_logs(self, databaseName):
         rows = []
-        database = self.client[databaseName]
-        collection = database['faces']
-        items = collection.find()
-        for item in items:
-            user = {'Name': item["name"], 'ID': item["id_num"], 'Date': item["date"].strftime("%Y-%m-%d"), 'Time': item["date"].strftime("%H:%M:%S")}
-            rows.append(user)
-        return rows[::-1]
+        client = self.client[databaseName]
+        database = client['faces']
+        collection = database.find()
+        for count, item in enumerate(collection):
+            columns = {key: value for (key, value) in item.items() if key != "_id"}
+            rows.append(columns)
+        header = [item for item in rows[0]]
+
+        return header, rows[::-1]
 
 if __name__ == "__main__":
     mydb = DB()
-    items = mydb.get_logs("detected_faces")
+    items = mydb.get_logs("recognized_faces")
